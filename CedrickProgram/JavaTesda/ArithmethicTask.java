7f6ea8eac7b904a0a5841365cbdc93fcbe4f6228
Cedrick John Barrera <151604849+C-Drick808@users.noreply.github.com>
2024-01-14 03:08:42 +0000

    Task complete arithmethic

---
+++ CedrickProgram/JavaTesda/ArithmethicTask.java
@@ -0,0 +1,26 @@
+/**
+ * ArithmethicTask
+ */
+public class ArithmethicTask {
+
+    public static void main(String[] args) {
+
+        int result = 1 + 2; // result is now 3
+        System.out.println(result);
+
+        result = result - 1; // result is now 2
+        System.out.println(result);
+
+        result = result * 2; // result is now 4
+        System.out.println(result);
+
+        result = result / 2; // result is now 2
+        System.out.println(result);
+
+        result = result + 8; // result is now 10
+        System.out.println(result);
+        result = result % 7; // result is now 3
+        System.out.println(result);
+        
+    }
+}
