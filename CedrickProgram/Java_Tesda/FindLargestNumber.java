8bb12c7b20bc65a23a8f76497a1c39b72e2f493b
Cedrick John Barrera <151604849+C-Drick808@users.noreply.github.com>
2024-01-14 08:45:14 +0000

    Task complete Find Largest Number

---
+++ CedrickProgram/Java_Tesda/FindLargestNumber.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+/**
+ * FindLargestNumber
+ */
+public class FindLargestNumber {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrNumber = new ArrayList<Integer>();
+        int num1 = 23,num2 = 12, num3 = 90;
+        arrNumber.add(num1);
+        arrNumber.add(num2);
+        arrNumber.add(num3);
+        
+        int maximum = arrNumber.get(0);
+        for (int i = 1; i < arrNumber.size(); i++) {
+            if (maximum < arrNumber.get(i))
+                maximum = arrNumber.get(i);
+        }
+
+        System.out.println("Largest number: " + (maximum));
+    }
+}
