5552805948cf10264c07848208b88491e8c7e2ee
Cedrick John Barrera <151604849+C-Drick808@users.noreply.github.com>
2024-01-15 08:58:12 +0000

    Identifying the missing display statement for the result 10 and completing the task using compound assignments.

--- CedrickProgram/Java_Tesda/ArithmethicTask.java
+++ CedrickProgram/Java_Tesda/ArithmethicTask.java
@@ -1,3 +1,5 @@
+package CedrickProgram.Java_Tesda;
+
 /**
  * ArithmethicTask
  */
@@ -5,22 +7,22 @@ public class ArithmethicTask {
 
     public static void main(String[] args) {
 
-        int result = 1 + 2; // result is now 3
+        int result = 1; // result is now 3
+        System.out.println(result+=2);
+
+        result -= 1; // result is now 2
         System.out.println(result);
 
-        result = result - 1; // result is now 2
+        result *= 2; // result is now 4
         System.out.println(result);
 
-        result = result * 2; // result is now 4
+        result /= 2; // result is now 2
         System.out.println(result);
 
-        result = result / 2; // result is now 2
-        System.out.println(result);
 
-        result = result + 8; // result is now 10
+        result += 8; // result is now 10
         System.out.println(result);
-        result = result % 7; // result is now 3
+        result %= 7; // result is now 3
         System.out.println(result);
-        
     }
 }
