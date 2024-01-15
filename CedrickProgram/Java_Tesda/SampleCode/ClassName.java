ac8b17f5f9625c62464c9a308d0dc0079f537560
Cedrick John Barrera <151604849+C-Drick808@users.noreply.github.com>
2024-01-15 07:39:47 +0000

    .

--- CedrickProgram/Java_Tesda/SampleCode/ClassName.java
+++ CedrickProgram/Java_Tesda/SampleCode/ClassName.java
@@ -1,16 +1,14 @@
 package CedrickProgram.Java_Tesda.SampleCode;
 
+import java.util.Arrays;
+
 public class ClassName {
     public static void main(String[] args) {
-        String name[] = new String[2];
+        String[] strings = {"10","9","100"};
+        Arrays.sort(strings);
+        for(String string : strings)
+            System.out.println(string + " ");
 
-        String[] strings = {"stringValue"};
-        Object[] objects = strings;
-        String[] againStrings = (String[])objects;
-        //againStrings[0] = new StringBuilder();
-        objects[0] = new StringBuilder(); 
-
-        System.out.println("This is strings" + strings);
 
 
     }
