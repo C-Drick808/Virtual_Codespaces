c96c21c5201b9dd78730a7d0d4aa5396a1747ddf
Cedrick John Barrera <151604849+C-Drick808@users.noreply.github.com>
2024-01-17 05:24:54 +0000

    fixing

--- CedrickProgram/Java_Tesda/FindLargestNumTask.java
+++ CedrickProgram/Java_Tesda/FindLargestNumTask.java
@@ -11,21 +11,28 @@ public class FindLargestNumTask {
         
         Scanner input = new Scanner(System.in);
         ArrayList<Integer> arrayNumber = new ArrayList<Integer>();
-
-        System.out.println("Enter a number: ");
-        int 
-
-        int num1 = 23,num2 = 12, num3 = 90;
-        arrNumber.add(num1);
-        arrNumber.add(num2);
-        arrNumber.add(num3);
         
-        int maximum = arrNumber.get(0);
-        for (int i = 1; i < arrNumber.size(); i++) {
-            if (maximum < arrNumber.get(i))
-                maximum = arrNumber.get(i);
+        System.out.print("Enter a First number: ");
+        int num1 = input.nextInt();
+        arrayNumber.add(num1);
+        System.out.print("Enter a Second number: ");
+        int num2 = input.nextInt();
+        arrayNumber.add(num2);
+        System.out.print("Enter a Third number: ");
+        int num3 = input.nextInt();
+        arrayNumber.add(num3);
+        input.close();
+
+        int maximum = arrayNumber.get(0);
+        for (int i = 1; i < arrayNumber.size(); i++) {
+            if (maximum < arrayNumber.get(i)){
+                maximum = arrayNumber.get(i);
+                System.out.println("The largest number is : " + maximum);
+            }
         }
 
-        System.out.println("Largest number: " + (maximum));
+        if (arrayNumber.equals()) {
+            System.out.println("All the numbers are equal");
+        }
     }
 }
