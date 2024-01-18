831f1a4605e6032688a6f9258b75aeab1c160c44
Cedrick John Barrera <151604849+C-Drick808@users.noreply.github.com>
2024-01-18 07:57:13 +0000

    Creating new task StringBuilder

---
+++ CedrickProgram/Java_Tesda/StringBuilderTask.java
@@ -0,0 +1,19 @@
+package CedrickProgram.Java_Tesda;
+
+import java.util.Scanner;
+
+/**
+ * StringBuilderTask
+ */
+public class StringBuilderTask {
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a word");
+        String typedWord = input.nextLine();
+
+
+
+    }
+}
