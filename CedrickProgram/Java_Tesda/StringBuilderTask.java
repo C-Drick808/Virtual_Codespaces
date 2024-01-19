402d6c251e4ab681c8506607cf028a5ed39db803
Cedrick John Barrera <151604849+C-Drick808@users.noreply.github.com>
2024-01-19 00:11:09 +0000

    String Builder task done

--- CedrickProgram/Java_Tesda/StringBuilderTask.java
+++ CedrickProgram/Java_Tesda/StringBuilderTask.java
@@ -13,27 +13,38 @@ public class StringBuilderTask {
         System.out.print("Enter a word: ");
         String typedWord = input.nextLine();
         StringBuilder sbtypeWord = new StringBuilder(typedWord);
+        input.close();
     
         int lenghtOfWord = sbtypeWord.length();
         int firstCharPosition = (lenghtOfWord - lenghtOfWord);
         int lastCharPosition = (lenghtOfWord - 1);
-        char firstCharacterWord = sbtypeWord.charAt(firstCharPosition);
-        char lastCharacterWord = sbtypeWord.charAt(lastCharPosition);
+        char firstCharacterWord = typedWord.charAt(firstCharPosition);
+        char lastCharacterWord = typedWord.charAt(lastCharPosition);
         int indexStringA = sbtypeWord.indexOf("a");
+        int substring1Position = (lenghtOfWord - lenghtOfWord) + 3;
+        int substring2Position = (lenghtOfWord - lenghtOfWord) + 7;
+        String substringIndex = typedWord.substring(substring1Position,substring2Position);
+        StringBuilder placeLastPos = sbtypeWord.append("123");
+        StringBuilder placeFourthPos = sbtypeWord.insert(4,"xyz");
+        StringBuilder del2Pos4 = sbtypeWord.delete(2, 4);
+        StringBuilder delPos8 = sbtypeWord.deleteCharAt(8);
+        StringBuilder reversedTypedWord = sbtypeWord.reverse();
 
 
 
-        
+
         System.out.println("Print the length of the string. "+lenghtOfWord);
         System.out.println("Print the first character of the string. "+firstCharacterWord);
         System.out.println("Print the last character of the string. "+lastCharacterWord);
         System.out.println("Print the index of the first occurrence of the letter 'a' in the string. "+indexStringA);
-        System.out.println("Print the substring of the string from index 3 to index 6.");
-        System.out.println("Append the string \"123\" to the end of the string.");
-        System.out.println("Insert the string \"xyz\" at index 4.");
-        System.out.println("Delete the substring from index 2 to index 4.");
-        System.out.println("Delete the character at index 8.");
-        System.out.println("Reverse the string and print it.");
+        System.out.println("Print the substring of the string from index 3 to index 6. "+substringIndex);
+        System.out.println("Append the string \"123\" to the end of the string. "+placeLastPos);
+        System.out.println("Insert the string \"xyz\" at index 4. "+placeFourthPos);
+        System.out.println("Delete the substring from index 2 to index 4. "+del2Pos4);
+        System.out.println("Delete the character at index 8. "+delPos8);
+        System.out.println("Reverse the string and print it. "+reversedTypedWord); 
+
+        
 
 
 
